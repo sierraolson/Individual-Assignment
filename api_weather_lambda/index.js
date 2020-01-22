047eb94a4e3068ad2017e8e087e2f7ad8600046c
James Jiang <jamesjiang@gatech.edu>
2020-01-21 22:51:49 -0500

    included functionality to support getting weather given lat and lon

--- api_weather_lambda/index.js
+++ api_weather_lambda/index.js
@@ -4,7 +4,29 @@ exports.handler = async (event) => {
     }
 };
 
-const determineDress = event => {
+const request = require('request')
 
+const getWeather = function(lat, lon) {
+    let api_key = 'cf60835a202d9fe0cde90a932a4ff7ed';
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
+    request(url, function (err, response, body) {
+        if(err){
+          console.log('error:', error);
+        } else {
+            let weather = JSON.parse(body)
+            console.log(weather)
+            let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            console.log(message);
+        }
+    });
 }
 
+
+const determineDress = event => {
+    let lat = '33.7490'
+    let lon = '-84.3880';
+    getWeather(lat, lon)
+}
+
+determineDress()
